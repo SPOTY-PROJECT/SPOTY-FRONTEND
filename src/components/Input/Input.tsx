b25e68a942ff6b7da626db099fab58505ed82c88
BooKi <qnrl9034@naver.com>
2022-10-24 23:59:25 +0900

    Feat Input Type, ToolTip setting SPOTY-53

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,22 +1,42 @@
 import React from "react";
 import classNames from "classnames/bind";
 
+import { ToolTip } from "../ToolTip/ToolTip";
+import { PassIcon } from "../Icon/PassIcon/PassIcon";
+import { WaringIcon } from "../Icon/WaringIcon/WaringIcon";
+
 import styles from "./Input.module.scss";
 
 type InputProps = {
   label: string;
   placeHolder: string;
-  toolTip:boolean;
-  type: "normal" | "pass" | "warning"
+  toolTipContent?: string;
+  type: "normal" | "pass" | "warning",
+  content?: string
 };
 
 const cx = classNames.bind(styles);
 
-function Input({ label, placeHolder, toolTip, type }: InputProps) {
+function Input({ label, placeHolder, toolTipContent, type, content }: InputProps) {
   return (
     <div className={cx("Input")}>
-      <label className={cx("label")}>{label}</label>
-      <input className={cx("input")} placeholder={placeHolder}/>
+      <div className={cx("wrapper")}>
+        <label className={cx("label")}>{label}</label>
+        {toolTipContent && <ToolTip content={toolTipContent}/>}
+      </div>
+      <input className={cx("input", type)} placeholder={placeHolder}/>
+      {type === "pass" &&
+        <div className={cx("content", type)}>
+          <PassIcon/>
+          {content}
+        </div>
+      }
+      {type === "warning" &&
+        <div className={cx("content", type)}>
+          <WaringIcon/>
+          {content}
+        </div>
+      }
     </div>
   );
 }
