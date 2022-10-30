8bfa7629b0eb01bda1db9b2bf25eaac2dc2ec58d
BooKi <qnrl9034@naver.com>
2022-10-30 17:41:00 +0900

    Feat Add ToolTip to DropDown SPOTY-54

--- src/storybook/DropDown/DropDown.tsx
+++ src/storybook/DropDown/DropDown.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import classNames from "classnames/bind";
+
+import { ToolTip } from "../ToolTip/ToolTip";
 
 import styles from "./DropDown.module.scss";
 
@@ -7,37 +9,42 @@ type DropDownProps = {
   title: string;
   option: string[];
   placeHolder: string;
+  onChange: any;
+  toolTipContent?: ReactNode;
 };
 
 const cx = classNames.bind(styles);
 
-function DropDown({ title, option, placeHolder }: DropDownProps) {
+function DropDown({ title, option, placeHolder, onChange, toolTipContent }: DropDownProps) {
   const [value, setValue] = useState("");
   const [show, setShow] = useState(false);
   return (
     <div className={cx("DropDown")}>
-      <label className={cx("title")}>{title}</label>
+      <div className={cx("label-wrapper")}>
+        <label className={cx("title")}>{title}</label>
+        {toolTipContent && <ToolTip content={toolTipContent}/>}
+      </div>
       <div className={cx("input-wrapper")}>
-        <input className={cx("input")} placeholder={placeHolder} value={value && value}/>
+        <input className={cx("input")} placeholder={placeHolder} value={value && value} onChange={onChange}/>
         <svg className={cx("icon")} onClick={() => setShow(!show)} width="10" height="7" viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 1L4.96036 6L9 1" stroke="black" strokeLinecap="round"/>
         </svg>
+        {
+          show &&
+          <div className={cx("option-wrapper")}>
+            {
+              option.map((item) => {
+                return <div className={cx("option")} onClick={() => {
+                  setValue(item);
+                  setShow(!show);
+                }}>
+                  {item}
+                </div>;
+              })
+            }
+          </div>
+        }
       </div>
-      {
-        show &&
-        <div className={cx("option-wrapper")}>
-          {
-            option.map((item) => {
-              return <div className={cx("option")} onClick={() => {
-                setValue(item);
-                setShow(!show);
-              }}>
-                {item}
-              </div>;
-            })
-          }
-        </div>
-      }
     </div>
   );
 }
