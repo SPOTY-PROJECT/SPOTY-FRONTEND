eae3ebfed9591cd8e7a0a32f07e94a1ebbded7ce
BooKi <qnrl9034@naver.com>
2022-10-30 17:43:59 +0900

    Set Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { CheckEmail } from "./components/Register/CheckEmail/CheckEmail";
+import { BaseData } from "./components/Register/BaseData/BaseData";
+import { Register } from "./pages/Register/Register";
+import { ExtraData } from "./components/Register/ExtraData/ExtraData";
+import { CheckNumber } from "./components/Register/CheckNumber/CheckNumber";
+import { Finish } from "./components/Register/Finish/Finish";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<CheckEmail/>}/>
+        <Route path={"/"} element={<Register/>}>
+          <Route path={""} element={<BaseData/>}/>
+          <Route path={"check"} element={<CheckNumber/>}/>
+          <Route path={"extra"} element={<ExtraData/>}/>
+          <Route path={"finish"} element={<Finish/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
